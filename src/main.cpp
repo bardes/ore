cbc7950541e9eb0d8f69a659542ca75896a78890
Paulo Bades <paulobardes@ig.com.br>
2012-04-30 02:00:07 -0300

    Work done in Map.cpp and Utils.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "core/Utils.hpp"
+#include "core/Map.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+	ore::Map test;
+	test.Load("./lvl-1.tmx");
+//	std::string b64("Qb==");
+//	std::vector<Byte> decoded;
+//	base64decode(b64, decoded);
+	return 0;
+}
